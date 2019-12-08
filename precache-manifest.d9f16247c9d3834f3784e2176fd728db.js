deaaa19ecfb28329f183862ece277e0ea1f98510
Marta Gajewska <gajewska.marta16@gmail.com>
2019-12-08 15:23:25 +0100

    Updates

---
+++ precache-manifest.d9f16247c9d3834f3784e2176fd728db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52bf5c24458f41b5816300e84d98538",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "cb0788bc49cd79cb002b",
+    "url": "/weather-app/static/css/main.4997c4bf.chunk.css"
+  },
+  {
+    "revision": "cd2b798f1b4184cda3e5",
+    "url": "/weather-app/static/js/2.15f4c2ac.chunk.js"
+  },
+  {
+    "revision": "cb0788bc49cd79cb002b",
+    "url": "/weather-app/static/js/main.4501893d.chunk.js"
+  },
+  {
+    "revision": "20b7fe9aece0c845f972",
+    "url": "/weather-app/static/js/runtime-main.630a9fd3.js"
+  },
+  {
+    "revision": "5d0871f6f92afd1872312ec556253d9a",
+    "url": "/weather-app/static/media/stars-and-clouds-at-nighttime-1229042.5d0871f6.jpg"
+  }
+]);
