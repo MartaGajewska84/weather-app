4fccc091270da250bd17e09327c539ec20829275
Marta Gajewska <gajewska.marta16@gmail.com>
2019-12-08 12:50:43 +0100

    finished

--- weather-app/src/components/weather.js
+++ weather-app/src/components/weather.js
@@ -4,21 +4,28 @@ const Weather = ({city, cityName, weather, latitude, longitude, temp, tempMax, t
     
         return(
             
-          <div className="container mt-3 text-center text-light">
-              {city && <h3>{cityName}</h3>}
-              {city && <h4>{weather}</h4>}
-            <div className="mt-4">
-              {city && <h6>Latitude: {latitude}</h6>}
-              {city && <h6>Longitude: {longitude}</h6>}
+            <div className="container mt-3 text-center text-light">
+                
+                    {city && 
+                        <div>
+                            <h3>{cityName}</h3>
+                            <h4>{weather}</h4>
+                            <div className="mt-4">
+                                <h6>Latitude: {latitude}</h6>
+                                <h6>Longitude: {longitude}</h6>
+                            </div>
+                            <div>
+                                <h6>Temperature: {temp}&#176;C</h6>
+                                <h6>Max temperature: {tempMax}&#176;C</h6>
+                                <h6>Min temperature: {tempMin}&#176;C</h6>
+                                <h6>Humidity: {humidity}%</h6>
+                                <h6>Pressure: {pressure}hPa</h6>
+                                <h6>Wind: {wind}m/s</h6>
+                            </div>
+                        </div>}
+                    {error && <h3>Please enter a city name</h3>} 
+                
             </div>
-              {city && <h6>Temperature: {temp}&#176;C</h6>}
-              {city && <h6>Max temperature: {tempMax}&#176;C</h6>}
-              {city && <h6>Min temperature: {tempMin}&#176;C</h6>}
-              {city && <h6>Humidity: {humidity}%</h6>}
-              {city && <h6>Pressure: {pressure}hPa</h6>}
-              {city && <h6>Wind: {wind}m/s</h6>}
-              {error && <h3>Please enter a city name</h3>} 
-          </div>
           
         )
     
